9ec471d5f1df9caa336f8c47fda816ddfd7db0c5
SANEESH-06 <saneeshn6@gmail.com>
2026-03-02 16:10:25 +0530

    fix sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,11 +3,11 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: "https://blog.saneesh.shop",
+            url: "https://blog.saneesh.shop/",
             lastModified: new Date(),
         },
         {
-            url: "https://blog.saneesh.shop/create",
+            url: "https://blog.saneesh.shop/create/",
             lastModified: new Date(),
         },
     ];
